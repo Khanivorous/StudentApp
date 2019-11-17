1e1536ed0b5f1fbbcb71799a0b898a0d3b20023b
asos-taherul <taherul.khan@asos.com>
2019-11-17 17:55:27 +0000

    Added Scenarios to feature and step definitions. Cleaned up build.gradle to resolve dependency errors

--- src/test/java/StepDefinitions.java
+++ src/test/java/StepDefinitions.java
@@ -1,2 +1,47 @@
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class StepDefinitions {
+    @Given("I send a GET request to the students api at path {int}")
+    public void i_send_a_GET_request_to_the_students_api_at_path(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @When("I receive the response")
+    public void i_receive_the_response() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I print the students Barry and {int} to the command line")
+    public void i_print_the_students_Barry_and_to_the_command_line(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I print the students Sheila and {int} to the command line")
+    public void i_print_the_students_Sheila_and_to_the_command_line(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("I send a GET request to the students api")
+    public void i_send_a_GET_request_to_the_students_api() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I all the students Barry and {int}")
+    public void i_all_the_students_Barry_and(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("I all the students Sheila and {int}")
+    public void i_all_the_students_Sheila_and(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
 }
