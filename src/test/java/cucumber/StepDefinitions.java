a274c185e7d5e8262bcf004add4ba618129d76db
asos-taherul <taherul.khan@asos.com>
2019-11-24 20:06:04 +0000

    Mockwebserver wip

--- src/test/java/cucumber/StepDefinitions.java
+++ src/test/java/cucumber/StepDefinitions.java
@@ -2,11 +2,19 @@ package cucumber;
 
 import com.khanivrous.app.StudentService;
 import com.khanivrous.app.StudentServiceGenerator;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import okhttp3.mockwebserver.MockWebServer;
 
 public class StepDefinitions {
+
+    private MockWebServer mockWebServer;
+
+    @Before
+
+
     @Given("I send a GET request to the students api at path {string}")
     public void i_send_a_GET_request_to_the_students_api_at_path(String string) {
         StudentService service = StudentServiceGenerator.createService(StudentService.class);
