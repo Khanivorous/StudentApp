3747e2fbd9f6610aed93b29d6cb5045d00b4626e
asos-taherul <taherul.khan@asos.com>
2019-11-25 22:34:07 +0000

    Added All scenario tests. Added API service call for list of students

--- src/main/java/com/khanivrous/app/PrintStudentInformation.java
+++ src/main/java/com/khanivrous/app/PrintStudentInformation.java
@@ -1,10 +1,11 @@
 package com.khanivrous.app;
 
-import com.khanivrous.app.models.StudentModel;
+import com.khanivrous.app.models.Student;
 import retrofit2.Call;
 import retrofit2.Response;
 
 import java.io.IOException;
+import java.util.List;
 
 public class PrintStudentInformation {
     public static void main(String[] args) {
@@ -14,19 +15,37 @@ public class PrintStudentInformation {
     }
 
     public static String printStudentDetailsById(String id, StudentService service) {
-        Call<StudentModel> studentModelCall = service.getStudentById(id);
+        Call<Student> studentModelCall = service.getStudentById(id);
 
-        Response<StudentModel> response = null;
+        Response<Student> response = null;
         try {
             response = studentModelCall.execute();
         } catch (IOException e) {
             e.printStackTrace();
         }
-        StudentModel student = response.body();
+        Student student = response.body();
         String studentResponseString = "Name: " + student.getName() + ", Age: " + student.getAge();
         System.out.println(studentResponseString);
         return studentResponseString;
     }
 
+    public static String printAllStudentsDetails(StudentService service) {
+
+        Call<List<Student>> call = service.getAllStudents();
+        Response<List<Student>> response = null;
+        try {
+              response = call.execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String responseString = "";
+        List<Student> students = response.body();
+
+        for (int i = 0; i < students.size(); i++) {
+            responseString += "Name: "+students.get(i).getName() + ", Age: " +students.get(i).getAge()+"/n";
+        }
+        System.out.println(responseString);
+        return responseString;
+    }
 }
 
