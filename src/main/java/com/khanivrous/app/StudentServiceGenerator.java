baaf64385c6e93b2296d6357c314be73b24963ff
asos-taherul <taherul.khan@asos.com>
2019-11-28 00:09:54 +0000

    Amended base url to use http as per server endpoint StudentServiceGenerator class now uses httpclient correctly

--- src/main/java/com/khanivrous/app/StudentServiceGenerator.java
+++ src/main/java/com/khanivrous/app/StudentServiceGenerator.java
@@ -6,17 +6,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class StudentServiceGenerator {
 
-    private static final String BASE_URL = "https://localhost:9292/";
+    private static final String BASE_URL = "http://localhost:3000/";
 
-    private static Retrofit.Builder builder
-            = new Retrofit.Builder()
-            .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create());
+    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+    private static Retrofit retrofit = new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).client(httpClient.build()).build();
 
-    private static Retrofit retrofit = builder.build();
-
-    private static OkHttpClient.Builder httpClient
-            = new OkHttpClient.Builder();
 
     public static <S> S createService(Class<S> serviceClass) {
         return retrofit.create(serviceClass);
